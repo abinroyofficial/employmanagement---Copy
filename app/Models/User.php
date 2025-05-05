71ac51c72596984a24cb6c6069af33a07a285560
Abin Roy <abinroy4321@gmail.com>
2025-05-05 12:24:10 +0530

    new data added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,20 +7,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
-    
-    use HasFactory, Notifiable,HasRoles;
 
-    
+    use HasFactory, Notifiable, HasRoles, HasApiTokens;
+
+
     protected $fillable = [
         'name',
         'email',
         'password',
         'password_reset'
     ];
-    public function regulizations(){
+    public function regulizations()
+    {
         return $this->hasMany(Regulization::class);
     }
     public function userData()
@@ -28,20 +31,21 @@ class User extends Authenticatable
         return $this->hasOne(Manager::class);
     }
 
-    public function comments(){
+    public function comments()
+    {
         return $this->hasMany(Comment::class);
     }
 
-    
 
-   
+
+
     protected $hidden = [
         'password',
         'remember_token',
         'password_reset',
     ];
 
-    
+
     protected function casts(): array
     {
         return [
